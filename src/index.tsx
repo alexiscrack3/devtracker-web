343f170eefb9862d5fbcbec141061701d9b2adbd
Alexis Ortega <alexis.oc3@gmail.com>
2023-12-31 10:29:29 -0800

    Add global css file

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+import "./stylesheets/global.scss";
+
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
